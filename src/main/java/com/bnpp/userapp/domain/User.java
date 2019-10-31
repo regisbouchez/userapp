f6d52528f0b8e46ff1cd9ef6f82309785d478ed4
bougis <regis.bouchez@gmail.com>
2019-10-31 17:18:39 +0100

    avec PostgreSQL

--- src/main/java/com/bnpp/userapp/domain/User.java
+++ src/main/java/com/bnpp/userapp/domain/User.java
@@ -5,7 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@Entity
+@Entity (name = "USER11")
 public class User {
     @Id
     @GeneratedValue
